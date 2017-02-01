f9e5586027857c579adaf9dfa540abdb5b840428
Vladimir Roncevic <elektron.ronca@gmail.com>
2017-02-01 12:29:22 +0100

    Initial code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# gen_sh_module
+Generate shell module script for sh_util (Bash script).
+================================================================================
+
+The README is used to introduce the modules and provide instructions on
+how to install the modules, any machine dependencies it may have and any
+other information that should be provided before the modules are installed.
+
+INSTALLATION
+
+To install this set of modules type the following:
+
+	cp -R ~/gen_sh_module/bin/   /root/scripts/gen_sh_module/ver.1.0/
+
+	cp -R ~/gen_sh_module/conf/  /root/scripts/gen_sh_module/ver.1.0/
+
+	cp -R ~/gen_sh_module/log/   /root/scripts/gen_sh_module/ver.1.0/
+
+DEPENDENCIES
+
+This module requires these other modules and libraries:
+
+	sh_util modules are build blocks for bash applications.
+
+COPYRIGHT AND LICENCE
+
+Copyright (C) 2017 by https://github.com/vroncevic/gen_sh_module
+
+This tool is free software; you can redistribute it and/or modify
+it under the same terms as Bash itself, either Bash version 4.2.47 or,
+at your option, any later version of Bash 4 you may have available.
+
+![alt tag](https://raw.githubusercontent.com/vroncevic/gen_sh_module/master/bash_logo.png)
+![alt tag](https://raw.githubusercontent.com/vroncevic/gen_sh_module/master/linux_logo.jpg)
+
---
+++ bin/gen_sh_module.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+#
+# @brief   Generating Bash script module
+# @version ver.1.0
+# @date    Wed May 11 13:00:19 CEST 2016
+# @company Frobas IT Department, www.frobas.com 2016
+# @author  Vladimir Roncevic <vladimir.roncevic@frobas.com>
+#
+UTIL_ROOT=/root/scripts
+UTIL_VERSION=ver.1.0
+UTIL=${UTIL_ROOT}/sh_util/${UTIL_VERSION}
+UTIL_LOG=${UTIL}/log
+
+.	${UTIL}/bin/devel.sh
+.	${UTIL}/bin/usage.sh
+.	${UTIL}/bin/check_root.sh
+.	${UTIL}/bin/check_tool.sh
+.	${UTIL}/bin/logging.sh
+.	${UTIL}/bin/send_mail.sh
+.	${UTIL}/bin/load_conf.sh
+.	${UTIL}/bin/load_util_conf.sh
+.	${UTIL}/bin/progress_bar.sh
+
+GEN_SH_MODULE_TOOL=gen_sh_module
+GEN_SH_MODULE_VERSION=ver.1.0
+GEN_SH_MODULE_HOME=${UTIL_ROOT}/${GEN_SH_MODULE_TOOL}/${GEN_SH_MODULE_VERSION}
+GEN_SH_MODULE_CFG=${GEN_SH_MODULE_HOME}/conf/${GEN_SH_MODULE_TOOL}.cfg
+GEN_SH_MODULE_UTIL_CFG=${GEN_SH_MODULE_HOME}/conf/${GEN_SH_MODULE_TOOL}_util.cfg
+GEN_SH_MODULE_LOG=${GEN_SH_MODULE_HOME}/log
+
+declare -A GENSHM_USAGE=(
+	[USAGE_TOOL]="${GEN_SH_MODULE_TOOL}"
+	[USAGE_ARG1]="[NAME] Name of Bash module (file name)"
+	[USAGE_EX_PRE]="# Create FileCheck module"
+	[USAGE_EX]="${GEN_SH_MODULE_TOOL} FileCheck"
+)
+
+declare -A GENSHM_LOGGING=(
+	[LOG_TOOL]="${GEN_SH_MODULE_TOOL}"
+	[LOG_FLAG]="info"
+	[LOG_PATH]="${GEN_SH_MODULE_LOG}"
+	[LOG_MSGE]="None"
+)
+
+declare -A PB_STRUCTURE=(
+	[BW]=50
+	[MP]=100
+	[SLEEP]=0.01
+)
+
+TOOL_DEBUG="false"
+
+#
+# @brief  Main function 
+# @param  Value required name of Bash script module
+# @retval Function __gen_sh_module exit with integer value
+#			0   - tool finished with success operation 
+#			128 - missing argument(s) from cli 
+#			129 - failed to load tool script configuration from files 
+#
+# @usage
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# NAME="FileCheck"
+# __gen_sh_module "$NAME"
+#
+function __gen_sh_module() {
+	local NAME=$1
+	if [ -n "${NAME}" ]; then
+		local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL STATUS
+		MSG="Loading basic and util configuration!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_SH_MODULE_TOOL"
+		__progress_bar PB_STRUCTURE
+		declare -A config_gen_sh_module=()
+		__load_conf "$GEN_SH_MODULE_CFG" config_gen_sh_module
+		STATUS_CONF=$?
+		declare -A config_gen_sh_module_util=()
+		__load_util_conf "$GEN_SH_MODULE_UTIL_CFG" config_gen_sh_module_util
+		STATUS_CONF_UTIL=$?
+		declare -A STATUS_STRUCTURE=(
+			[1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL
+		)
+		__check_status STATUS_STRUCTURE
+		STATUS=$?
+		if [ $STATUS -eq $NOT_SUCCESS ]; then
+			MSG="Force exit!"
+			__info_debug_message_end "$MSG" "$FUNC" "$GEN_SH_MODULE_TOOL"
+			exit 129
+		fi
+		local DATE=$(date) VERSION=${config_gen_sh_module_util[VERSION]}
+		local COMPANY=${config_gen_sh_module_util[COMPANY]}
+		local AUTHOR=${config_gen_sh_module_util[AUTHOR]}
+		local MODULE_TEMPLATE=${config_gen_sh_module_util[MODULE_TEMPLATE]}
+		local HASH="#" TAB="	"
+		MSG="Generating shell module [${NAME}.sh]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_SH_MODULE_TOOL"
+		local UMNAME=$(echo ${NAME} | tr 'a-z' 'A-Z') SHMLINE
+		while read SHMLINE
+		do
+			eval echo "$SHMLINE" >> "${NAME}.sh"
+		done < "${GEN_SH_MODULE_HOME}/conf/${MODULE_TEMPLATE}"
+		MSG="Set owner!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_SH_MODULE_TOOL"
+		local USRID=${config_gen_sh_module_util[UID]}
+		local GRPID=${config_gen_sh_module_util[GID]}
+		eval "chown ${USRID}.${GRPID} \"${NAME}.sh\""
+		MSG="Set permission!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_SH_MODULE_TOOL"
+		eval "chmod 700 ${NAME}.sh"
+		__info_debug_message_end "Done" "$FUNC" "$GEN_SH_MODULE_TOOL"
+		exit 0
+	fi
+	__usage GENSHM_USAGE
+	exit 128
+}
+
+#
+# @brief   Main entry point
+# @param   Value required shell module name
+# @exitval Script tool gen_sh_module exit with integer value
+#			0   - tool finished with success operation 
+# 			127 - run tool script as root user from cli
+#			128 - missing argument(s) from cli 
+#			129 - failed to load tool script configuration from files 
+#
+printf "\n%s\n%s\n\n" "${GEN_SH_MODULE_TOOL} ${GEN_SH_MODULE_VERSION}" "`date`"
+__check_root
+STATUS=$?
+if [ $STATUS -eq $SUCCESS ]; then
+	__gen_sh_module $1
+fi
+
+exit 127
+
